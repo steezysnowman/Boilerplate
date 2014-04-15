4f4e0e011d941b07e706020cbdc576d65880f263
Brian Lai <bjlai@ucsd.edu>
2014-04-14 17:13:12 -0700

    added styling for display fb and twitter posts + routing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
+// Get all of our friend data
+var data = require('../data.json');
+
 exports.view = function(req, res) {
 	res.render('index');
 }
 
 exports.display = function(req, res) {
-	res.render('display');
+	res.render('display', data);
 }
